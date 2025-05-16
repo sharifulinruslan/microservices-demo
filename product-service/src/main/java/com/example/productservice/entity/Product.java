20cae9fd2559c914ac5c07fc8ae176f68b8cb934
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-16 15:09:37 +0400

    Add Product entity

---
+++ product-service/src/main/java/com/example/productservice/entity/Product.java
@@ -0,0 +1,22 @@
+package com.example.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private int stockQuantity;
+}
