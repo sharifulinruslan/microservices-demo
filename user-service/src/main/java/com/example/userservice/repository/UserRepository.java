07f7fc4e1fc092ea119d0edf8128494f9476b69d
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-16 13:35:58 +0400

    Change types in declaration UserRepository interface

--- user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.userservice.repository;
 
+import com.example.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository<Long, User> extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
