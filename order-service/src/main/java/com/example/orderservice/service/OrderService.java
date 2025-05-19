dc1242accde8c5ba8e98d95a73a096ab75527aac
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-19 16:03:15 +0400

    Add OrderService

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,67 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.UserDTO;
+import com.example.orderservice.entity.Order;
+import com.example.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+    @Value("${user-service.url}")
+    private String userServiceUrl;
+    @Value("${inventory-service.url}")
+    private String inventoryServiceUrl;
+
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository, WebClient.Builder webClientBuilder) {
+        this.orderRepository = orderRepository;
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    public void createOrder(Order order) {
+        UserDTO userDTO = webClientBuilder.build().get()
+                .uri(userServiceUrl + "/{id}", order.getId())
+                .retrieve()
+                .bodyToMono(UserDTO.class)
+                .block();
+
+        if (userDTO == null) {
+            System.err.println("User not found");
+        }
+
+        for (Long productId : order.getProductIds()) {
+            Boolean inStock = webClientBuilder.build().get()
+                    .uri(inventoryServiceUrl + "/inventory/product/{productId}/in-stock", productId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+
+            if (inStock == null) {
+                System.err.println("Product out of stock");
+            }
+        }
+
+        order.setStatus("CREATED");
+        orderRepository.save(order);
+    }
+
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public void updateOrderStatus(Long id, String status) {
+        Order order = getOrderById(id);
+        order.setStatus(status);
+        orderRepository.save(order);
+    }
+
+    public void deleteOrderById(Long id) {
+        orderRepository.deleteById(id);
+    }
+}
