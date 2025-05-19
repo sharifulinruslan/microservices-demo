47b47e72ddef00881fd5efd1854539cf5bee6cf3
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-19 15:46:44 +0400

    Add Order entity

---
+++ order-service/src/main/java/com/example/orderservice/entity/Order.java
@@ -0,0 +1,23 @@
+package com.example.orderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private List<Long> productIds;
+    private Double totalPrice;
+    private String status;
+}
