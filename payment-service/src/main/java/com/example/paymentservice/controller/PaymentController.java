16e4088c2124fff33408b24bf9079f37943281af
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-20 14:36:52 +0400

    Add PaymentController

---
+++ payment-service/src/main/java/com/example/paymentservice/controller/PaymentController.java
@@ -0,0 +1,48 @@
+package com.example.paymentservice.controller;
+
+import com.example.paymentservice.entity.Payment;
+import com.example.paymentservice.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping
+    public List<Payment> getAllPayments() {
+        return paymentService.getAllPayments();
+    }
+
+    @GetMapping("/{id}")
+    public Payment getPaymentById(@PathVariable Long id) {
+        return paymentService.getPaymentById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+        paymentService.createPayment(payment);
+        return ResponseEntity.ok(payment);
+    }
+
+    @PutMapping
+    public ResponseEntity<Payment> updatePayment(@RequestBody Payment payment) {
+        paymentService.updatePayment(payment);
+        return ResponseEntity.ok(payment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Payment> deletePayment(@PathVariable Long id) {
+        paymentService.deletePaymentById(id);
+        return ResponseEntity.ok(paymentService.getPaymentById(id));
+    }
+}
