025123fbfc96ce0e9033dc8ecc1684a6b60323a7
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-20 14:36:41 +0400

    Add order-service url field. Add update and delete Payment methods

--- payment-service/src/main/java/com/example/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/example/paymentservice/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.example.paymentservice.service;
 import com.example.paymentservice.entity.Payment;
 import com.example.paymentservice.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,8 @@ import java.util.List;
 @Service
 public class PaymentService {
     private final PaymentRepository paymentRepository;
+    @Value("${order-service.url}")
+    private String orderServiceUrl;
 
     @Autowired
     public PaymentService(PaymentRepository paymentRepository) {
@@ -30,5 +33,13 @@ public class PaymentService {
 
     public Payment getPaymentById(Long id) {
         return paymentRepository.findById(id).orElse(null);
+    }
+
+    public void deletePaymentById(Long id) {
+        paymentRepository.deleteById(id);
+    }
+
+    public void updatePayment(Payment payment) {
+        paymentRepository.save(payment);
     }
 }
