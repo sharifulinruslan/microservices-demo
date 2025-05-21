faf855c1e7828396813d390c1b9ab12c691d10ff
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-21 10:08:15 +0400

    Make User implementing Serializable

--- user-service/src/main/java/com/example/userservice/entity/User.java
+++ user-service/src/main/java/com/example/userservice/entity/User.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
