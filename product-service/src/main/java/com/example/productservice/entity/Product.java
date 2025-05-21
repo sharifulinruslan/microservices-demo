1d3e2c19c87b39244335ccaa21981e63a85ad025
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-21 11:25:25 +0400

    Make Product implementing Serializable

--- product-service/src/main/java/com/example/productservice/entity/Product.java
+++ product-service/src/main/java/com/example/productservice/entity/Product.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "products")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
+public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
