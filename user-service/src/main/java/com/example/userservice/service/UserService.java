3ece63a1563c48db0c860e7db47ac591ec23736a
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-21 10:09:12 +0400

    Add updateUser method. Add Cache annotations to work with redis

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -2,6 +2,9 @@ package com.example.userservice.service;
 
 import com.example.userservice.entity.User;
 import com.example.userservice.repository.UserRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +21,16 @@ public class UserService {
         userRepository.save(user);
     }
 
+    @CachePut(value = "users", key = "#user.id")
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Cacheable(value = "users", key = "#id")
     public User getUserById(Long id) {
         return (User) userRepository.findById(id).orElse(null);
     }
-
+    
     public User getUserByEmail(String email) {
         return (User) userRepository.findByEmail(email).orElse(null);
     }
@@ -30,6 +39,7 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    @CacheEvict(value = "users", key = "#id")
     public void deleteUser(Long id) {
         if (this.getUserById(id) != null) {
             userRepository.delete(this.getUserById(id));
