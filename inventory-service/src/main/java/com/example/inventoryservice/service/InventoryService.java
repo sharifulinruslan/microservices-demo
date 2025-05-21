57f537c6198722fb5d1568363998013ebd949977
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-21 13:08:18 +0400

    Add cache annotations

--- inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -5,6 +5,9 @@ import com.example.inventoryservice.entity.Inventory;
 import com.example.inventoryservice.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -40,15 +43,22 @@ public class InventoryService {
         return inventoryRepository.findAll();
     }
 
+    @Cacheable(value = "inventory", key = "#id")
+    public Inventory getInventoryById(Long id) {
+        return inventoryRepository.findById(id).orElse(null);
+    }
+
     public Inventory getInventoryByProductId(Long productId) {
         return inventoryRepository.findByProductId(productId).orElse(null);
     }
 
-    public void updateInventory(Inventory inventory) {
+    @CachePut(value = "inventory", key = "#inventory.id")
+    public Inventory updateInventory(Inventory inventory) {
         inventory.setInStock(inventory.getQuantity() > 0);
-        inventoryRepository.save(inventory);
+        return inventoryRepository.save(inventory);
     }
 
+    @CacheEvict(value = "inventory", key = "#id")
     public void deleteInventoryById(Long id) {
         inventoryRepository.deleteById(id);
     }
