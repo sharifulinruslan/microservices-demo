9a54805c7378b6b13beb764bfd7440e790df6913
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-21 11:25:50 +0400

    Add update method. Add cache annotations

--- product-service/src/main/java/com/example/productservice/service/ProductService.java
+++ product-service/src/main/java/com/example/productservice/service/ProductService.java
@@ -3,6 +3,9 @@ package com.example.productservice.service;
 import com.example.productservice.entity.Product;
 import com.example.productservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,19 +23,26 @@ public class ProductService {
         productRepository.save(product);
     }
 
-    public Product getProductById(long id){
+    @CachePut(value = "products", key = "#product.id")
+    public void updateProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    @Cacheable(value = "products", key = "#id")
+    public Product getProductById(long id) {
         return productRepository.findById(id).orElse(null);
     }
 
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    public List<Product> getProductsByCategory(String category){
+    public List<Product> getProductsByCategory(String category) {
         return productRepository.findByCategory(category);
     }
 
-    public void deleteProductById(long id){
+    @CacheEvict(value = "products", key = "#id")
+    public void deleteProductById(long id) {
         productRepository.deleteById(id);
     }
 }
