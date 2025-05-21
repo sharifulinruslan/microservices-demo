5f59d3cff8a3a76948e945c243408bf3bce56569
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-05-21 13:07:11 +0400

    Make Inventory implementing Serializable

--- inventory-service/src/main/java/com/example/inventoryservice/entity/Inventory.java
+++ inventory-service/src/main/java/com/example/inventoryservice/entity/Inventory.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "inventory")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Inventory {
+public class Inventory implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
