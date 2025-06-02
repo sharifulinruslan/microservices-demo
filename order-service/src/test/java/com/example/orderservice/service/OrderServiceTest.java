d17a32b1d9dbf09ebcbc1b830c0d0df3719a88ef
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-06-02 10:59:56 +0400

    OrderServiceTest init

---
+++ order-service/src/test/java/com/example/orderservice/service/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.orderservice.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(
+        MockitoExtension.class
+)
+class OrderServiceTest {
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void getOrderById() {
+    }
+
+    @Test
+    void updateOrderStatus() {
+    }
+
+    @Test
+    void deleteOrderById() {
+    }
+}
