1370581c1487c47d593e637f301d57570d84396f
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-06-04 11:36:27 +0400

    InventoryControllerTest init

---
+++ inventory-service/src/test/java/com/example/inventoryservice/controller/InventoryControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.inventoryservice.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InventoryControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllInventory() {
+    }
+
+    @Test
+    void getInventoryById() {
+    }
+
+    @Test
+    void getInventoryByProductId() {
+    }
+
+    @Test
+    void getInventoryByProductIdInStock() {
+    }
+
+    @Test
+    void createInventory() {
+    }
+
+    @Test
+    void updateInventory() {
+    }
+
+    @Test
+    void deleteInventory() {
+    }
+}
