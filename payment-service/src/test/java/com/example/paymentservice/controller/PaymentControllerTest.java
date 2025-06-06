e4a388b43967a147626b451b4e587751733bfeff
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-06-06 14:08:32 +0400

    Init PaymentServiceTest and PaymentControllerTest

---
+++ payment-service/src/test/java/com/example/paymentservice/controller/PaymentControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.paymentservice.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PaymentControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllPayments() {
+    }
+
+    @Test
+    void getPaymentById() {
+    }
+
+    @Test
+    void createPayment() {
+    }
+
+    @Test
+    void updatePayment() {
+    }
+
+    @Test
+    void deletePayment() {
+    }
+}
---
+++ payment-service/src/test/java/com/example/paymentservice/service/PaymentServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.paymentservice.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PaymentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createPayment() {
+    }
+
+    @Test
+    void getAllPayments() {
+    }
+
+    @Test
+    void getPaymentById() {
+    }
+
+    @Test
+    void deletePaymentById() {
+    }
+
+    @Test
+    void updatePayment() {
+    }
+}
